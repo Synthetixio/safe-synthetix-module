7aed02f69243d6be390eeb0b18205dac665a7900
Kevin Mao <kevin.mao@student.unsw.edu.au>
2023-10-15 16:43:09 +1100

    deploy and initalise safes

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -17,7 +17,7 @@ contract DeployScript is Script {
     address internal account;
 
     // TODO change this to 0 when in prod
-    uint256 internal customSalt = 4;
+    uint256 internal customSalt = 0;
     string internal result;
 
     mapping(string => address) internal register;
