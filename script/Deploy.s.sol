00e6d68040d7d4a46379a97dbeb4b56681510b15
Kevin Mao <kevin.mao@student.unsw.edu.au>
2023-10-24 21:52:07 +1100

    new

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -17,7 +17,7 @@ contract DeployScript is Script {
     address internal account;
 
     // TODO change this to 0 when in prod
-    uint256 internal customSalt = 0;
+    uint256 internal customSalt = 8;
     string internal result;
 
     mapping(string => address) internal register;
